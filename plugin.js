5d0b0ea7176c6faa35d486ab17be552d19a4ff9c
hanafy0 <30327222+hanay0@users.noreply.github.com>
2020-08-14 04:42:36 +0200

    adding some tasks
    - adding function for current slide
    -  adding slide's numbers to the slide

--- plugin.js
+++ plugin.js
@@ -5,7 +5,7 @@ const sliderImages = Array.from(document.querySelectorAll('.slider-container img
 const sliderImagesCount = sliderImages.length;
 
 // setting current slide to start form it
-const currentSlide = 1;
+const currentSlide = 2;
 
 // number of the current slide element 
 const slideNumber = document.getElementById('slide-number');
@@ -16,29 +16,62 @@ const prev = document.getElementById('prev'),
 
 
 
+// create hte main ul element which contains the numbers of slides [paginations]
+let paginationElement = document.createElement('ul');
 
+// give an id to pagination element
+paginationElement.setAttribute('id', 'pagination-ul')
 
+// creating list items based on array length [how many slides or images exists]
+for(let i = 1; i <= sliderImagesCount; i++){
+    // create li inside the previous ul
+    let paginationsItems = document.createElement('li');
+ 
+    // set custom attribute 
+    paginationsItems.setAttribute('data-index', i);
 
-      // next slide function
-let previousSlide = () => {
-    console.log('previous in log');
+    // set item content
+    paginationsItems.appendChild(document.createTextNode(i));
+
+    // append items to the main ul list
+    paginationElement.appendChild(paginationsItems)
+
 }
+ 
+ // adding created ul element to the main page
 
-// previous slide function
-let nextSlide = () => {
-    console.log('next in log');
-}
+document.getElementById("indicators").appendChild(paginationElement);
 
-// handling click events of previous and next buttons
+// handle the events of previous and next buttons
 prev.addEventListener('click', previousSlide);
 next.addEventListener('click', nextSlide);
 
+// get the new created ul
+let paginationCreatedUl = document.getElementById('pagination-ul');
+
+// trigger the checker function
+theChecker();
+// previous function
+function previousSlide() {
+    console.log('Previous');
+}
+
+// next function
+function nextSlide() {
+    console.log('Next');
+}
 
 
+// create checker function 
+function theChecker(){
 
+    // set the slide number
+    slideNumber.textContent = `slide # ${currentSlide} of ${sliderImagesCount}`;
 
+    // set active class on the current element
+    sliderImages[currentSlide - 1].classList.add('active');
 
+    // set active pagination
+    paginationCreatedUl.children[currentSlide - 1].classList.add('active');
 
-
-
-
+}
